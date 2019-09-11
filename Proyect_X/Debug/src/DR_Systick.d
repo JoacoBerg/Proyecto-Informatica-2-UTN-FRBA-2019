ecad30eeaa4d8c4a196dc3380fd90145191a2358
GiF3000 <gif.kapo@gmail.com>
2019-09-10 21:19:35 -0300

    volando el debug

--- Proyect_X/Debug/src/DR_Systick.d
+++
@@ -1,7 +0,0 @@
-src/DR_Systick.o src/DR_Systick.d: ../src/DR_Systick.c \
- C:\Users\gif30\OneDrive\ -\ est.frba.utn.edu.ar\MCUXpressoIDE_10.2.1_795\workspace\Proyect_INFO2_2019\Proyect_X\inc/DR_Systick.h \
- C:\Users\gif30\OneDrive\ -\ est.frba.utn.edu.ar\MCUXpressoIDE_10.2.1_795\workspace\Proyect_INFO2_2019\Proyect_X\inc/DR_tipos.h
-
-C:\Users\gif30\OneDrive\ -\ est.frba.utn.edu.ar\MCUXpressoIDE_10.2.1_795\workspace\Proyect_INFO2_2019\Proyect_X\inc/DR_Systick.h:
-
-C:\Users\gif30\OneDrive\ -\ est.frba.utn.edu.ar\MCUXpressoIDE_10.2.1_795\workspace\Proyect_INFO2_2019\Proyect_X\inc/DR_tipos.h:
