af67909b25af720d153f2fa4d58a679ac9a30bd5
GCRIS <GCRIS@10.5.73.182>
2019-09-25 18:22:39 -0300

    testing

--- Proyect_X/src/main.c
+++ Proyect_X/src/main.c
@@ -21,8 +21,11 @@
 #include <Maq_FollowTheLine.h>
 #include "Maq_Giro.h"
 #include "Boton.h"
+#include "DR_Servo.h"
 // TODO: insert other definitions and declarations here
 
+void testing(void);
+void testing_servos(void);
 
 int main(void) {
 
@@ -34,23 +37,33 @@ int main(void) {
     Tanks_Init();
     InitIR();
     Boton_init();
-
+    //Servo init no existe, ya que el tanque se encarga de eso
     while(1) {
-    	int i=0;
-    	//LecturaIRs(IRx4);
-        if(i == 1){
-        	if(Maq_FollowTheLine())
-        		i = 2;
-        }
-       	else if( i == 0){
-       		if(Maq_Giro(IZQ))
-       			i = 1;
-       	}
-        else if(i == 2){
-        	if(BOTON)
-        		i = 0;
-        }
+    	testing();
     }
-
     return 0 ;
+}
+
+
+void testing_servos(void){
+	for(int i=0;i<500000;i++)
+		Servo_Abierto();
+	for(int i=0;i<500000;i++)
+	    Servo_Cerrado();
+}
+void testing(void){
+	static int i=0;
+	//LecturaIRs(IRx4);
+    if(i == 1){
+    	if(Maq_FollowTheLine())
+    		i = 2;
+    }
+   	else if( i == 0){
+   		if(Maq_Giro(IZQ))
+   			i = 1;
+   	}
+    else if(i == 2){
+    	if(BOTON)
+    		i = 0;
+    }
 }
