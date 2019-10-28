83c17d7af848cff587d814a19d5566072e58bdb7
JoaquinBergmann <JoaquinBergmann@192.168.0.101>
2019-10-27 22:11:16 -0300

    Se codea Maq GRAL y Maq Base

--- Proyect_X/inc/MAQ/Maq_Base.h
+++ Proyect_X/inc/MAQ/Maq_Base.h
@@ -8,6 +8,9 @@
 #ifndef MAQ_MAQ_BASE_H_
 #define MAQ_MAQ_BASE_H_
 
+#include "DR_tipos.h"
+#include "BUFFER_MAQ.h"
 
+uint8_t Maq_Base(void);
 
 #endif /* MAQ_MAQ_BASE_H_ */
--- Proyect_X/src/MAQ/Maq_GRAL.c
+++ Proyect_X/src/MAQ/Maq_GRAL.c
@@ -1,36 +1,9 @@
 #include "Maq_GRAL.h"
 #include "Maq_Giro.h"
+#include "Maq_Base.h"
 #include "Maq_FollowTheLine.h"
 #include "DR_tipos.h"
-
-
-//Declaracion de estados
-#define 	SELECTION	0
-#define 	BASE	    1
-#define 	CAJA	    2
-#define 	GIRO_IZQ	3
-#define 	GIRO_DER	4
-#define 	FORWARD	    5
-
-
-
-void Push_list_estados(uint8_t dato);
-int32_t Pop_list_estados(void);
-
-
-uint8_t Maq_Base(void){
-	static uint32_t i= 500000;
-
-	if(i){
-		i--;
-		return 0;
-	}
-	else{
-		i=500000;
-		return 1;
-	}
-}
-
+#include "BUFFER_MAQ.h"
 
 
 uint8_t Maq_Caja(void){
@@ -84,35 +57,3 @@ void Maq_General()
 }
 
 
-
-#define TAM_BUFFER_list_estados 500
-uint8_t lista_estados [TAM_BUFFER_list_estados];
-uint8_t in_estados = 0;
-uint8_t out_estados = 0;
-
-
-
-
-void Push_list_estados(uint8_t dato){
-	lista_estados[in_estados] = dato;
-	in_estados++;
-	if (in_estados>= TAM_BUFFER_list_estados)
-		in_estados = 0;
-}
-
-
-
-
-int32_t Pop_list_estados(void){
-	int32_t dato ;
-
-	if(in_estados == out_estados) //the list is empty
-		return -1;
-	else{
-		dato = lista_estados[out_estados];
-		out_estados++;
-		if (out_estados >= TAM_BUFFER_list_estados)
-			out_estados = 0;
-	}
-	return dato;
-}
