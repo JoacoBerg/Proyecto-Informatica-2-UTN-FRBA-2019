db5f0cef526e8f9527e29eb1ec8f010650ea67b9
I860540 <I860540@BUEN34388305A.amer.global.corp.sap>
2019-12-07 21:28:25 -0300

    probando leds

--- Proyect_X/src/main.c
+++ Proyect_X/src/main.c
@@ -16,15 +16,51 @@
 #include <DR_Inicializacion.h>
 #include "PR_Timers.h"
 #include "Maq_GRAL.h"
-//#include "testings.h"
+
+#include "DR_GPIO.h"
+#include "DR_PINSEL.h"
+
+#include "testings.h"
 
 //#include "Maq_FollowTheLine.h"
+
+//#define LED1 2,6
+//#define LED2 2,7
+//#define LED3 2,8
+//#define LED4 2,10
 
 int main(void) {
 
    	Inicializacion ();
    	//test_Maq_GRAL_init();
 
+   	/*
+   	SetPinsel(LED1, PINSEL_GPIO);
+	SetPinMode(LED1, PINMODE_PULLUP);
+	SetPinDir(LED1, PINDIR_OUTPUT);
+
+	SetPinsel(LED2, PINSEL_GPIO);
+	SetPinMode(LED2, PINMODE_PULLUP);
+	SetPinDir(LED2, PINDIR_OUTPUT);
+
+	SetPinsel(LED3, PINSEL_GPIO);
+	SetPinMode(LED3, PINMODE_PULLUP);
+	SetPinDir(LED3, PINDIR_OUTPUT);
+
+	SetPinsel(LED4, PINSEL_GPIO);
+	SetPinMode(LED4, PINMODE_PULLUP);
+	SetPinDir(LED4, PINDIR_OUTPUT);
+
+	SetPin(LED1, ON);
+	SetPin(LED2, ON);
+	SetPin(LED3, ON);
+	SetPin(LED4, ON);
+
+	SetPin(LED1, OFF);
+	SetPin(LED2, OFF);
+	SetPin(LED3, OFF);
+	SetPin(LED4, OFF);
+*/
    	while(1) {
     	TimerEvent();
     	Maq_General();
